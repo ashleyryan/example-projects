d475858be9ff03e28d554f40eea1059bd776799a
Ashley Ryan <asryan@vmware.com>
2022-09-30 14:41:49 -0400

    use regular react import and transform it

--- react-tsx/web-test-runner.config.mjs
+++ react-tsx/web-test-runner.config.mjs
@@ -1,8 +1,14 @@
+import rollupCommonjs from '@rollup/plugin-commonjs';
 import { esbuildPlugin } from "@web/dev-server-esbuild";
+import { fromRollup } from '@web/dev-server-rollup';
+
+const commonjs = fromRollup(rollupCommonjs);
+
 
 export default {
   nodeResolve: true,
   plugins: [
+    commonjs(),
     esbuildPlugin({ tsx: true, jsxFactory: "React.createElement", jsxFragment: "Fragment" }),
   ],
 };
